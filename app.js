c336ea14a9bb8395b92e52251785ff7c69b0f11e
Rifhan Akram <rifhana@99x.lk>
2017-06-23 05:13:32 +0530

    - add root route

--- app.js
+++ app.js
@@ -10,9 +10,15 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
+app.get('/',function(req,res) {
+    res.status(200).json({
+        "message":"success"
+    });
+});
 app.get('/jobs/all',function(req,res) {
     res.status(200).json(jobs);
 })
+
 
 app.listen(5000,function(){
     console.log('server running');
