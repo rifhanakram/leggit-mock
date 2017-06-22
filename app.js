7d23d1ae3f9b761f862353b8d55872f56e8a2939
Rifhan Akram <rifhana@99x.lk>
2017-06-23 05:09:25 +0530

    - minor changes

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-'use strict';
-
-const express = require('express');
-const app = express();
-const jobs = require('./api/jobs');
 
 
-app.use((req, res, next) => {
+var express = require('express');
+var app = express();
+var jobs = require('./api/jobs');
+
+
+app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
-app.get('/jobs/all',(req,res) => {
+app.get('/jobs/all',function(req,res) {
     res.status(200).json(jobs);
 })
 
-app.listen(5000,()=>{
+app.listen(5000,function(){
     console.log('server running');
 });
