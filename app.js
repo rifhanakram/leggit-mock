cd6e013bd80a8bf59afc7b1f4f36e8efcb3f579d
Rifhan Akram <rifhana@99x.lk>
2017-07-07 09:51:52 +0530

    add completed job list

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var app = express();
 var jobs = require('./api/jobs');
+var completedJobs = require('./api/jobs-completed')
 
 
 app.use(function(req, res, next) {
@@ -18,6 +19,12 @@ app.get('/',function(req,res) {
 app.get('/jobs/all',function(req,res) {
     res.status(200).json({data: jobs});
 })
+
+app.get('/jobs/completed',function(req,res) {
+    res.status(200).json({data: completedJobs});
+})
+
+
 
 
 app.listen(process.env.PORT || 3000,function(){
