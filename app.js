f2a1bf65751e2da8dc56eec691fc2832617d0d9a
Rifhan Akram <rifhana@99x.lk>
2017-07-18 12:15:27 +0530

    - add query param

--- app.js
+++ app.js
@@ -7,26 +7,26 @@ var completedJobs = require('./api/jobs-completed')
 
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  next();
+res.header('Access-Control-Allow-Origin', '*');
+res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+next();
 });
 app.get('/',function(req,res) {
-    res.status(200).json({
-        "message":"success"
-    });
+res.status(200).json({
+    "message":"success"
 });
-app.get('/jobs/all',function(req,res) {
-    res.status(200).json({data: jobs});
-})
+});
 
 app.get('/jobs/completed',function(req,res) {
+if(req.query.job_status == 4) 
     res.status(200).json({data: completedJobs});
+else if(req.query.job_status == 0) 
+    res.status(200).json({data: jobs})
 })
 
 
 
 
 app.listen(process.env.PORT || 3000,function(){
-    console.log('server running');
+console.log('server running');
 });
